0613d9af7b2ed1d199f11cf0ad1d8622fb0c1db6
Petr Šimek <petr@ptrsimek.com>
2019-09-05 14:02:30 +0200

    MethodImpl, cleanup

--- Converter/IntEnumConverter.cs
+++ Converter/IntEnumConverter.cs
@@ -1,33 +1,30 @@
 namespace Converter
 {
     using System;
-    using System.Linq.Expressions;
     using System.Reflection.Emit;
-
-    public static class Q
-    {
-        
-    }
+    using System.Runtime.CompilerServices;
 
     public static class IntEnumConverter<T> where T : Enum
     {
-        private static readonly Func<int, T> ConvertFunc = CreateConverter();
+        private static readonly Func<int, T> ConvertFuncIntToT = CreateConverterIntToT();
         
-        private static readonly Func<T, int> ConvertFuncX = CreateConverterX();
+        private static readonly Func<T, int> ConvertFuncTToInt = CreateConverterTToInt();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Convert(int e)
         {
-            var result = ConvertFunc(e);
+            var result = ConvertFuncIntToT(e);
             return result;
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Convert(T e)
         {
-            var result = ConvertFuncX(e);
+            var result = ConvertFuncTToInt(e);
             return result;
         }
 
-        private static Func<int, T> CreateConverter()
+        private static Func<int, T> CreateConverterIntToT()
         {
             var m = new DynamicMethod(string.Empty, typeof(T), new[] { typeof(int) }, true);
 
@@ -39,9 +36,9 @@ namespace Converter
             return result;
         }
         
-        private static Func<T, int> CreateConverterX()
+        private static Func<T, int> CreateConverterTToInt()
         {
-            var m = new DynamicMethod(string.Empty, typeof(T), new[] { typeof(T) }, false);
+            var m = new DynamicMethod(string.Empty, typeof(T), new[] { typeof(T) }, true);
             
             var il = m.GetILGenerator();
             il.Emit(OpCodes.Ldarg_0);
@@ -49,13 +46,6 @@ namespace Converter
             
             var result = (Func<T, int>)m.CreateDelegate(typeof(Func<T, int>));
             return result;
-            
-            //var arg = Expression.Parameter(typeof(T), "arg");
-            //
-            //var f = Expression.Lambda<Func<T, int>>(Expression.Convert(arg, typeof(int)), arg);
-            //var result = f.Compile()
-            //
-            // return result;
         }
     }
 }
--- Converter/PerformanceTest.cs
+++ Converter/PerformanceTest.cs
@@ -1,8 +1,7 @@
 namespace Converter
 {
     using System;
-    using System.Linq.Expressions;
-    using System.Reflection.Emit;
+    using System.Runtime.CompilerServices;
 
     using BenchmarkDotNet.Attributes;
 
@@ -12,10 +11,6 @@ namespace Converter
         private const int TestsCount = 10000;
 
         private readonly TestEnum enumValue;
-
-        private static readonly Func<TestEnum, int> ConverterDynamic = CreateConverterDynamic<TestEnum>();
-        
-        private static readonly Func<TestEnum, int> ConverterExpession = CreateConverterDynamic<TestEnum>();
 
         public PerformanceTest()
         {
@@ -27,53 +22,16 @@ namespace Converter
         {
             var sum = 0;
             var e = this.enumValue;
-            var c = ConverterDynamic;
             
             for (var i = 0; i < TestsCount; i++)
             {
-                sum += c(e);
+                sum += ConvertToInt(e);
             }
             
             GC.KeepAlive(sum);
         }
-        
+
         [Benchmark]
-        public void EnumToIntExpression()
-        {
-            var sum = 0;
-            var e = this.enumValue;
-            var c = ConverterExpession;
-            
-            for (var i = 0; i < TestsCount; i++)
-            {
-                sum += c(e);
-            }
-            
-            GC.KeepAlive(sum);
-        }
-
-        private static Func<T, int> CreateConverterDynamic<T>()
-        {
-            var m = new DynamicMethod(string.Empty, typeof(T), new[] { typeof(T) }, typeof(PerformanceTest), false);
-            
-            var il = m.GetILGenerator();
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Ret);
-            
-            var result = (Func<T, int>)m.CreateDelegate(typeof(Func<T, int>));
-            return result;
-        }
-
-        private static Func<T, int> CreateConverterExpression<T>()
-        {
-            var arg = Expression.Parameter(typeof(T), "arg");
-            var f = Expression.Lambda<Func<T, int>>(Expression.Convert(arg, typeof(int)), arg);
-            var result = f.Compile();
-
-            return result;
-        }
-
-        //[Benchmark]
         public void EnumToIntCast()
         {
             var sum = 0;
@@ -87,16 +45,11 @@ namespace Converter
             GC.KeepAlive(sum);
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static int ConvertToInt<T>(T val) where T : Enum
         {
             var result = IntEnumConverter<T>.Convert(val);
             return result;
         }
-        
-        // private static T ConvertToEnum<T>(int val) where T : Enum
-        // {
-        //     var result = IntEnumConverter<T>.Convert(val);
-        //     return result;
-        // }
     }
 }
